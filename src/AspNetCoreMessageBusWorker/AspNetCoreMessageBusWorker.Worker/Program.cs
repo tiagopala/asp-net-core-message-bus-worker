8e95b6a2e5e9c3d811b9881ee8c0ad202bb4c9d1
Tiago Pala <tiago_pala@outlook.com>
2021-12-24 10:49:17 -0300

    Criação worker default

---
+++ src/AspNetCoreMessageBusWorker/AspNetCoreMessageBusWorker.Worker/Program.cs
@@ -0,0 +1,10 @@
+using AspNetCoreMessageBusWorker.Worker.Workers;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
