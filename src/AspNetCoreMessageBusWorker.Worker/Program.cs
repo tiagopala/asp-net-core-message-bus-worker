c917062d96564f8d494ec66b892a8954791aa47e
Tiago Pala <tiago_pala@outlook.com>
2021-12-24 16:57:42 -0300

    Início implementação do worker

--- src/AspNetCoreMessageBusWorker.Worker/Program.cs
+++ src/AspNetCoreMessageBusWorker.Worker/Program.cs
@@ -1,8 +1,18 @@
 using AspNetCoreMessageBusWorker.Worker.Workers;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((hostContext, services) =>
     {
+        IConfiguration configuration = hostContext.Configuration;
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(hostContext.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true)
+            .AddEnvironmentVariables();
+
+        var options = configuration.GetAWSOptions();
+        services.AddAWSService<IAmazonSQS>(options);
         services.AddHostedService<Worker>();
     })
     .Build();
--- src/AspNetCoreMessageBusWorker.Worker/Workers/Worker.cs
+++ src/AspNetCoreMessageBusWorker.Worker/Workers/Worker.cs
@@ -1,20 +1,52 @@
+using Amazon.SQS.Model;
+using AspNetCoreMessageBusWorker.Domain.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace AspNetCoreMessageBusWorker.Worker.Workers;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IAmazonSQS _sqsClient;
+    private readonly IConfiguration _configuration;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(
+        ILogger<Worker> logger,
+        IAmazonSQS sqsClient,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _sqsClient = sqsClient;
+        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            var messages = await PullMessages();
+
+            if (messages.Any())
+                messages.ToList().ForEach(message => Process(message));
         }
+    }
+
+    private async Task<IEnumerable<Message>> PullMessages()
+    {
+        var pullingResponse = await _sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+        {
+            QueueUrl = _configuration["AWS:SQS:QueueUrl"],
+            MaxNumberOfMessages = int.TryParse(_configuration["AWS:SQS:MaxNumberOfMessages"], out int result) ? result : 1
+        });
+
+        return pullingResponse.Messages;
+    }
+
+    private void Process(Message message)
+    {
+        var person = JsonSerializer.Deserialize<Person>(message.Body);
+
+        _logger.LogInformation("Person Processed", person);
     }
 }
